b7a19695a93ba36a6469c75a95772badefe77519
eliasvansickle <eliasvansickle@gmail.com>
2015-06-05 16:36:45 -0700

    add class, after, and append FINISHED

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ $(document).ready(function() {
 	$("button.one").click(function() {
 		$("h1.one").addClass("onestyle");
 	});
+	$("button.two").click(function() {
+		$("p.two").after("Hello Kitty");
+	});
+	$("button.three").click(function() {
+		$("p.three").append("Hello Kitty");
+	});
 
-}); 
+
+
+});
